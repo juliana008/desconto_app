2fff13e7d994ffd09f212cd775ce9175c385e4ed
Juliana Barretto Vilas Boas <julibarrettovb@gmail.com>
2026-09-24 10:36:20 -0300

    implementação da camada de controller, database e serviços

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
-from src.models.desconto import DescontoVIP, DescontoNormal, DescontoPremium
+from src.controllers.pedido_controller import PedidoController
+from src.repositories.pedido_repository import PedidoRepository
+from src.services.pedido_service import PedidoService
+from src.database.connection import DatabaseConnection
 from src.models.pedido import Pedido
-from src.services.pedido_servico import PedidoService
+from src.models.desconto import DescontoVIP, DescontoNormal, DescontoPremium
+
+
+
 
 if __name__=="__main__":
+    # Criação de objetos
+    database = DatabaseConnection()
+    repo = PedidoRepository(database)
+    repo = PedidoRepository()
+    service = PedidoService(repo)
+    controller = PedidoController(service)
     
-    servico = PedidoService()
+    # Criando um pedido com desconto
     
-    """criando pedidos e aplicando descontos"""
-    pedido1 = Pedido("Cliente A", DescontoNormal())
+    pedido1 = Pedido("Jonso", DescontoNormal())
     pedido1.valor_original = 100.0 # Definindo o valor original do pedido
     
-    pedido2 = Pedido("Cliente B", DescontoVIP())
-    pedido2.valor_original = 200.0 # Definindo o valor original do pedido
+    pedido2 = Pedido("Vitin", DescontoVIP())
+    pedido2.valor_original = 100.0 # Definindo o valor original do pedido
     
-    pedido3 = Pedido("Cliente C", DescontoPremium())
-    pedido3.valor_original = 300.0 # Definindo o valor original do pedido
+    pedido3 = Pedido("Titila", DescontoPremium())
+    pedido3.valor_original = 100.0 # Definindo o valor original do pedido
     
-    # Criar pedidos, aplicar descontos e processar os pedidos
-       
-    servico.adicionar_pedido(pedido1)
-    servico.adicionar_pedido(pedido2)
-    servico.adicionar_pedido(pedido3)
+        
+    # Salvamento dos pedidos no repositório
+    controller.adicionar_pedido(pedido1)
+    controller.adicionar_pedido(pedido2)
+    controller.adicionar_pedido(pedido3)
     
-    servico.processar_pedidos()
+    # Processando os pedidos
+    controller.processar_pedidos()
---
+++ src/repositories/pedido_repository.py
@@ -0,0 +1,14 @@
+from src.models.pedido import Pedido
+from src.database.connection import DatabaseConnection
+
+class PedidoRepository:
+    """Classe de repositório para armazenar e gerenciar pedidos."""
+    
+    def __init__(self, database: DatabaseConnection):
+        self.database = database
+        
+    def adicionar_pedido(self, pedido: Pedido):
+        self.database.append(pedido)
+        
+    def listar_pedidos(self):
+        return self.database.pedidos
