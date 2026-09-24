93447c081879112f78fbf11d91e6fd81a6ed2271
Juliana Barretto Vilas Boas <julibarrettovb@gmail.com>
2026-09-24 10:03:32 -0300

    feat: adição de novos pedidos implementando a camada de serviços

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from src.models.desconto import DescontoVIP
+from src.models.desconto import DescontoVIP, DescontoNormal, DescontoPremium
 from src.models.pedido import Pedido
 from src.services.pedido_servico import PedidoService
+
 if __name__=="__main__":
-    pedido = Pedido("Leonardo", DescontoVIP())
-    pedido_servico = PedidoService()
     
-    valor_final = pedido.valor_final(100)
+    servico = PedidoService()
     
-    pedido_servico.adicionar_pedido(pedido)
-    pedido_servico.processar_pedidos()
+    """criando pedidos e aplicando descontos"""
+    pedido1 = Pedido("Cliente A", DescontoNormal())
+    pedido1.valor_original = 100.0 # Definindo o valor original do pedido
+    
+    pedido2 = Pedido("Cliente B", DescontoVIP())
+    pedido2.valor_original = 200.0 # Definindo o valor original do pedido
+    
+    pedido3 = Pedido("Cliente C", DescontoPremium())
+    pedido3.valor_original = 300.0 # Definindo o valor original do pedido
+    
+    # Criar pedidos, aplicar descontos e processar os pedidos
+       
+    servico.adicionar_pedido(pedido1)
+    servico.adicionar_pedido(pedido2)
+    servico.adicionar_pedido(pedido3)
+    
+    servico.processar_pedidos()
